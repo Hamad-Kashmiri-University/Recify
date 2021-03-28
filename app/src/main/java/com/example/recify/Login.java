6ad9dff9fa3949208313ae33c89d581c02d68403
Hamad <kashmirh@coventry.ac.uk>
2021-03-28 22:06:37 +0100

    login Linear View added

--- app/src/main/java/com/example/recify/Login.java
+++ app/src/main/java/com/example/recify/Login.java
@@ -1,6 +1,7 @@
 package com.example.recify;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +9,7 @@ public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         overridePendingTransition(R.anim.fadein, R.anim.fadeout);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
