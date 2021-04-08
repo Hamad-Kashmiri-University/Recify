85fa63c603d22d8191056f15b800db9574953d91
Hamad <kashmirh@coventry.ac.uk>
2021-04-08 22:14:15 +0100

    google play services installed and confgured

--- app/src/main/java/com/example/recify/NearbySpots.java
+++ app/src/main/java/com/example/recify/NearbySpots.java
@@ -1,8 +1,14 @@
 package com.example.recify;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class NearbySpots extends AppCompatActivity {
 
@@ -10,5 +16,25 @@ public class NearbySpots extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nearby_spots);
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        int errorCode = GoogleApiAvailability.getInstance()
+                .isGooglePlayServicesAvailable(this);
+        if (errorCode!= ConnectionResult.SUCCESS) {
+            Dialog error = GoogleApiAvailability.getInstance()
+                    .getErrorDialog(this, errorCode, errorCode, new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialogInterface) {
+                            Toast.makeText(NearbySpots.this, "no services", Toast.LENGTH_LONG).show();
+                            finish();
+                        }
+                    });
+                error.show();
+        }else {
+            Toast.makeText(NearbySpots.this, "Google Services Connected", Toast.LENGTH_LONG).show();
+        }
     }
 }
