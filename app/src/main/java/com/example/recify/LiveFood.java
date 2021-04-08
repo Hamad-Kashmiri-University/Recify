11c8a79a72c70009fecd81adf35c91537fd40c2f
Hamad <kashmirh@coventry.ac.uk>
2021-04-08 20:06:55 +0100

    google speech functional

--- app/src/main/java/com/example/recify/LiveFood.java
+++ app/src/main/java/com/example/recify/LiveFood.java
@@ -1,19 +1,57 @@
 package com.example.recify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class LiveFood extends AppCompatActivity {
 
+    //vars
+    ImageView speechFirst;
+    EditText speechText;
+    // check result to see if result is same value as passed in intent
+    private static final int RESULT = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_food);
 
-        // Enable AR-related functionality on ARCore supported devices only.
+        //hooks
+        speechFirst = findViewById(R.id.speechFirst);
+        speechText = findViewById(R.id.speechText);
+
+
+        speechFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent speech = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                //informs recogniser which
+                speech.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                speech.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak a food to search for recipes");
+                startActivityForResult(speech, RESULT);
+            }
+        });
 
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == RESULT && resultCode == RESULT_OK){
+            ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            speechText.setText(results.get(0).toString());
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
+
