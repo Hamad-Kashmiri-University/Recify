7d3ceffe666512a868ee14a5aa056c6555d48363
Hamad <kashmirh@coventry.ac.uk>
2021-04-09 03:36:28 +0100

    current user loc auto loaded with marker plus permissions check

--- app/src/main/java/com/example/recify/NearbySpots.java
+++ app/src/main/java/com/example/recify/NearbySpots.java
@@ -1,14 +1,37 @@
 package com.example.recify;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -16,9 +39,16 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
-public class NearbySpots extends AppCompatActivity implements OnMapReadyCallback {
+public class NearbySpots extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     boolean permissions;
+    GoogleMap myMap;
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
+    private int GPS_REQUEST_CODE = 9001;
+    private Location prevLoc;
+    private Marker userMarker;
+    private static final int REQUEST_USER_LOCATION_CODE = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +56,31 @@ public class NearbySpots extends AppCompatActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_nearby_spots);
         permissionValidate();
 
-        if(permissions){
-            SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-            supportMapFragment.getMapAsync(this);
+        if (Build.VERSION.SDK_INT >- Build.VERSION_CODES.M){
+            checkUserLoc();
         }
+
+        if (permissions) {
+            if(isGPS()) {
+                SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                supportMapFragment.getMapAsync(this);
+            }
+        }
+    }
+
+    private boolean isGPS() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if(enabled) {
+            return true;
+        } else {
+            AlertDialog alertDialog = new AlertDialog.Builder(this).setTitle("GPS permissions").setMessage("GPS is required").setPositiveButton("yes", ((dialogInterface, i) -> {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(intent, GPS_REQUEST_CODE);
+            })).setCancelable(false).show();
+        }
+        return false;
     }
 
     private void permissionValidate() {
@@ -54,9 +105,123 @@ public class NearbySpots extends AppCompatActivity implements OnMapReadyCallback
     }
 
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        myMap = googleMap;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            buildGoogleClient();
+            myMap.setMyLocationEnabled(true);
+        }
+        }
+
+        public boolean checkUserLoc() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_USER_LOCATION_CODE);
+                }
+                else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_USER_LOCATION_CODE);
+                }
+                return false;
+            }
+        else {
+            return true;
+        }
+        }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode)
+        {
+            case REQUEST_USER_LOCATION_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                        if (googleApiClient == null){
+                            buildGoogleClient();
+                        }
+                        myMap.setMyLocationEnabled(true);
+                    }
+                }
+                else {
+                    Toast.makeText(this, "Permission was not granted", Toast.LENGTH_SHORT).show();
+                }
+                return;
+        }
+    }
+
+    protected synchronized void buildGoogleClient() {
+
+        googleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+
+        googleApiClient.connect();
+
+        }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest,  this);
+        }
 
     }
 
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==GPS_REQUEST_CODE){
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            if(enabled){
+               Toast.makeText(this, "gps enabled", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "gps NOT enabled", Toast.LENGTH_SHORT).show();
+            }
+
+    }
+}
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        prevLoc = location;
+
+        if (userMarker != null)
+        {
+            userMarker.remove();
+        }
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        Log.d("positions", "onLocationChanged: test" + latLng);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Your Location");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+
+        userMarker = myMap.addMarker(markerOptions);
+
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        // amount zoomed by
+        myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 13.0f));
+
+        if (googleApiClient!= null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+        }
+    }
 }
