94db219a17571f8c2b3506460cb5c13df098cb03
Hamad <kashmirh@coventry.ac.uk>
2021-04-16 17:43:55 +0100

    xml for 2 pages for sqlite added

--- app/src/main/java/com/example/recify/UploadRecipe.java
+++ app/src/main/java/com/example/recify/UploadRecipe.java
@@ -1,14 +1,34 @@
 package com.example.recify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class UploadRecipe extends AppCompatActivity {
+
+    //vars
+    private FloatingActionButton addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_recipe);
+
+        //hooks
+        addButton = findViewById(R.id.addButton);
+
+        //onclicklisterner
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // go to activity for adding a record to swlite
+                startActivity(new Intent(UploadRecipe.this, AddUpdateRecipe.class));
+
+            }
+        });
     }
 }
