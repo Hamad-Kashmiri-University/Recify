1113b6f97fdf9d999816512e3892253ad21ac94c
Hamad <kashmirh@coventry.ac.uk>
2021-04-16 18:25:33 +0100

    sql class initialsid helper class next

--- app/src/main/java/com/example/recify/DatabaseConstants.java
+++ app/src/main/java/com/example/recify/DatabaseConstants.java
@@ -1,4 +1,34 @@
 package com.example.recify;
 
 public class DatabaseConstants {
+
+    //database name
+    public static final String DB_NAME = "RecifyDB";
+    //version
+    public static final int DB_VERSION = 1;
+    //name of model
+    public static final String TABLE_NAME = "recipes";
+    //fields
+    public static final String C_ID = "ID;"
+    public static final String C_NAME = "NAME";
+    public static final String C_IMAGE = "IMAGE";
+    public static final String C_TIME = "TIME";
+    public static final String C_INSTRUCTIONS = "INSTRUCTIONS";
+    public static final String C_INGREDIENTS = "INGREDIENTS";
+    public static final String C_ADDED = "ADD_TIME";
+    public static final String C_UPDATED = "UPDATED_TIME";
+
+    //query to create
+    public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " ("
+            + C_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + C_NAME + " TEXT,"
+            + C_IMAGE + " TEXT,"
+            + C_TIME + " INTEGER,"
+            + C_INSTRUCTIONS + " TEXT,"
+            + C_INGREDIENTS + " TEXT,"
+            + C_ADDED + " TEXT,"
+            + C_UPDATED +  "TEXT"
+            + ")";
 }
+
+
